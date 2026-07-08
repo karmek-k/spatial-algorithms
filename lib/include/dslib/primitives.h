ad4419c44a92f14a230028d97df116a3d34aace7
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2026-07-08 19:27:34 +0200

    feat: start point_side

---
+++ lib/include/dslib/primitives.h
@@ -0,0 +1,9 @@
+#ifndef DSLIB_PRIMITIVES_H
+#define DSLIB_PRIMITIVES_H
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+#endif
---
+++ programs/point_side/point_side.c
@@ -0,0 +1,16 @@
+#include <dslib/primitives.h>
+
+int main(void) {
+    Point line_from = { 0, 0 };
+    Point line_to = { 1, 1 };
+
+    const int N = 2;
+    Point pts[N] = {
+        { 0, 1 },
+        { 1, 0 }
+    };
+
+    for (int i = 0; i < N; ++i) {
+        
+    }
+}
